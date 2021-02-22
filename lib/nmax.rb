c9382f5b6805f2dfac93b1e0d524e028935728a2
Alexey Kuznetsov <altairk91@yandex.ru>
2021-02-22 09:37:42 +0500

    Create lib.

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,6 +1,16 @@
-require "nmax/version"
-require "nmax/railtie"
-
 module Nmax
-  # Your code goes here...
+  autoload :VERSION, 'nmax/version'
+
+  def self.get(stdin:, count:)
+    chars = stdin.each_char
+    grouped_numbers = chars.chunk { |c| c =~ /\d/ }
+
+    numbers = grouped_numbers.inject([]) do |memo, (_, digits)|
+      memo << digits.join.to_i
+      memo = memo.sort.last(count) if memo.size > count
+      memo
+    end
+
+    numbers.sort.last(count)
+  end
 end
