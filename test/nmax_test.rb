5467c5ce2e50ab2a016f41ab9f554977b21fbfa7
Alexey Kuznetsov <altairk91@yandex.ru>
2021-02-22 09:47:49 +0500

    Create tests.

--- test/nmax_test.rb
+++ test/nmax_test.rb
@@ -1,7 +1,24 @@
-require "test_helper"
+require_relative 'test_helper'
+require 'tempfile'
 
-class NmaxTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Nmax::VERSION
+describe Nmax do
+  it 'should get 2 max numbers from readed file' do
+    assert_equal `cat #{Bundler.root}/test/fixtures/text_with_numbers.txt | #{Bundler.root}/bin/nmax 2}`, "22\n30\n"
+  end
+
+  def memstats
+    `ps -o rss= #{Process.pid}`.strip.to_i
+  end
+
+  it 'should not load file to process memory' do
+    file = Tempfile.new('tmp')
+    file.write(('0'..'z').to_a.*(50_000).sample(1_000_000).join)
+    file.rewind
+
+    starting_memory = memstats
+
+    Nmax.get(stdin: file, count: 5)
+
+    assert memstats < starting_memory + 6000, 'Calculation has consumed more than 6MB'
   end
 end
